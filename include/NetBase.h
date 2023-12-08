0e77f4a76fd2796405741df8242c124ada3cf784
AkiharaYanagi <150650129+AkiharaYanagi@users.noreply.github.com>
2023-12-08 17:10:52 +0900

    別環境でのコミット
    テスト

--- include/NetBase.h
+++ include/NetBase.h
@@ -56,7 +56,9 @@ namespace GAME
 		~NetBase ();
 
 //		static void ThreadFunc ();		//スレッド関数
+#if 0
 		virtual void StartThread ( std::function < void () > threadFunc );		//スレッドを開始
+#endif // 0
 		virtual void End ();		//スレッドを終了
 
 		//許可状態の取得と変更(ロック付)
